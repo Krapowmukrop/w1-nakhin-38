82eb3692e3a5fc1f1414736c7fe45a286ce7ddc9
admin <thiraphat.phansanit@e-tech.ac.th>
2026-02-14 13:08:26 +0700

    Update

--- App.tsx
+++ App.tsx
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text, Image, ScrollView} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+export  default function App(){
+  return(
+
+    <ScrollView>
+      {}
+    <View style={{flexDirection:"row", alignItems:"center"}}>
+      <Image source={require("./assets/senku4.gif")} style={{width: 100, height: 100, borderRadius: 50, margin: 20}}   />
+    <View>
+        <Text style={{fontSize:18, fontWeight:"600"}}>Senku Ishigami</Text>
+        <Text><Text style={{ fontWeight:"600"}}>12</Text> posts  <Text> 23.8M</Text> Followers <Text>  1</Text> Following </Text>
+        <Text><Text style={{ fontWeight:"600"}}>#ฝันร้ายวิทยาศาสตร์</Text><Text></Text><Text></Text></Text>
     </View>
-  );
-}
+    </View>
+    
+     <ScrollView horizontal={true} showsVerticalScrollIndicator={false}>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <View>
+        <Image source={require("./assets/senku3.gif")} style={{width: 80, height:80, borderRadius: 40, borderWidth: 3, borderColor:"lightgray"}}/>
+        <Text style={{textAlign:"center"}}>Senku </Text>
+      </View>
+      <View>
+        <Image source={require("./assets/senku5.gif")} style={{width: 80, height:80, borderRadius: 40, borderWidth: 3, borderColor:"lightgray"}}/>
+        <Text style={{textAlign:"center"}}>Senku</Text>
+      </View>x
+      <View>
+        <Image source={require("./assets/senku6.gif")} style={{width: 80, height:80, borderRadius: 40, borderWidth: 3, borderColor:"lightgray"}}/>
+        <Text style={{textAlign:"center"}}>Senku</Text>
+      </View>
+      <View>
+        <Image source={require("./assets/senku7.gif")} style={{width: 80, height:80, borderRadius: 40, borderWidth: 3, borderColor:"lightgray"}}/>
+        <Text style={{textAlign:"center"}}>Senku</Text>
+      </View>
+      <View>
+        <Image source={require("./assets/senku8.gif")} style={{width: 80, height:80, borderRadius: 40, borderWidth: 3, borderColor:"lightgray"}}/>
+        <Text style={{textAlign:"center"}}>Senku</Text>
+      </View>
+      <View>
+        <Image source={require("./assets/senku9.gif")} style={{width: 80, height:80, borderRadius: 40, borderWidth: 3, borderColor:"lightgray"}}/>
+        <Text style={{textAlign:"center"}}>Senku</Text>
+        </View>
+
+     </ScrollView>
+
+     <View style={{flexDirection:"row", flexWrap:"wrap", gap: 5, marginTop:20}}>
+      <Image source={require("./assets/senku10.gif")} style={{width:"32%", height:200}}/>
+      <Image source={require("./assets/senku11.gif")} style={{width:"32%", height:200}}/>
+      <Image source={require("./assets/senku12.gif")} style={{width:"32%", height:200}}/>
+      <Image source={require("./assets/senku13.gif")} style={{width:"32%", height:200}}/>
+      <Image source={require("./assets/senku14.gif")} style={{width:"32%", height:200}}/>
+      <Image source={require("./assets/senku15.gif")} style={{width:"32%", height:200}}/>
+      <Image source={require("./assets/senku16.gif")} style={{width:"32%", height:200}}/>
+      <Image source={require("./assets/senku17.gif")} style={{width:"32%", height:200}}/>
+      <Image source={require("./assets/senku18.gif")} style={{width:"32%", height:200}}/>
+      <Image source={require("./assets/senku19.gif")} style={{width:"32%", height:200}}/>
+      <Image source={require("./assets/senku20.gif")} style={{width:"32%", height:200}}/>
+      <Image source={require("./assets/senku21.gif")} style={{width:"32%", height:200}}/>
+     </View>
+
+
+    </ScrollView>
+
+    
+
+   
+  )
+}
